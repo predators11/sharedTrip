2bfb20fa0f76cc78fc085b83e1c7e744733ac651
Vasil Vasilev <novopc@abv.bg>
2022-09-12 19:28:25 +0300

    Initial commit

---
+++ SharedTrip/Controllers/TripsController.cs
@@ -0,0 +1,82 @@
+﻿using BasicWebServer.Server.Attributes;
+using BasicWebServer.Server.Controllers;
+using BasicWebServer.Server.HTTP;
+using SharedTrip.Contracts;
+using SharedTrip.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SharedTrip.Controllers
+{
+    public class TripsController : Controller
+    {
+        private readonly ITripService tripService;
+        public TripsController(
+            Request request,
+            ITripService _tripService)
+            : base(request)
+        {
+            tripService = _tripService;
+        }
+
+        [Authorize]
+        public Response Add() => View();
+
+        [HttpPost]
+        [Authorize]
+        public Response Add(TripViewModel model)
+        {
+            var (isValid, errors) = tripService.ValidateModel(model);
+
+            if (!isValid)
+            {
+                return View(errors, "/Error");
+            }
+
+            try
+            {
+                tripService.AddTrip(model);
+            }
+            catch (Exception ex)
+            {
+                return View(new List<ErrorViewModel>() { new ErrorViewModel("Unexpected error") }, "/Error");
+            }
+
+            return Redirect("/Trips/All");
+        }
+
+        [Authorize]
+        public Response All()
+        {
+            IEnumerable<TripListViewModel> trips = tripService.GetAllTrips();
+
+            return View(trips);
+        }
+
+        [Authorize]
+        public Response Details(string tripId)
+        {
+            TripDetailsViewModel tripDetailsViewModel = tripService.GetTripDetails(tripId);
+
+            return View(tripDetailsViewModel);
+        }
+
+        public Response AddUserToTrip(string tripId)
+        {
+            try
+            {
+                tripService.AddUserToTrip(tripId, User.Id);
+            }
+            catch (ArgumentException aex)
+            {
+                return View(new List<ErrorViewModel>() { new ErrorViewModel(aex.Message) }, "/Error");
+            }
+            catch (Exception)
+            {
+                return View(new List<ErrorViewModel>() { new ErrorViewModel("Unexpected error") }, "/Error");
+            }
+
+            return Redirect("/Trips/All");
+        }
+    }
+}
